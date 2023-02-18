c4ed05bf477775c81041f78e87f649e6e792db12
smhasanjamil <smhasanjamil135@gmail.com>
2023-02-18 15:29:34 +0600

    Added first card button click and output show in the table

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let serial = 0;
+/*
+*================First Card==================
+*/
+document.getElementById('firts-card').addEventListener('click', function () {
+    serial += 1;
+
+    const productName = document.getElementById('product-name').innerText;
+    const firstInput = document.getElementById('first-card-first-input').value;
+    const secondInput = document.getElementById('first-card-second-input').value;
+    const result = 0.5 * parseFloat(firstInput) * parseFloat(secondInput);
+    const convertToMeterSquire = document.write = '<span style="background-color: blue; color: white; padding: 5px; border-radius: 5px">Convert to M<sup>2</sup></span>';
+    
+    displayData(productName, firstInput, secondInput, result, convertToMeterSquire);
+});
+
+
+/*
+*================Function Data Display==================
+*/
+//Common function to display data
+function displayData(nameOfProduct, valueOfFirstInput, ValueOfSecondInput, ResultOfCalculation, convertToMeterSquire) {
+    const container = document.getElementById('table-container');
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+        <td>${serial}</td>
+        <td>${nameOfProduct}</td>
+        <td>${ResultOfCalculation} <span>cm <sup>2</sup> </span></td>
+        <td>${convertToMeterSquire}</td>    
+    `;
+    container.appendChild(tr);
+}
